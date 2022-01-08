1dd7eeebb27fad36ccf2f83fa2c12697fedf91f2
Andrey <dev.andr@yandex.com>
2022-01-08 22:50:56 +0500

    fix: изменить Main для использования класса проверки

--- src/ru/yandex/practicum/tracker/Main.java
+++ src/ru/yandex/practicum/tracker/Main.java
@@ -1,7 +1,14 @@
 package ru.yandex.practicum.tracker;
 
+import ru.yandex.practicum.tracker.manager.InMemoryTasksManager;
+import ru.yandex.practicum.tracker.manager.Managers;
+import ru.yandex.practicum.tracker.tests.Generator;
+
 public class Main {
     public static void main(String[] args) {
-        new InMemoryTasksManager();
+
+        InMemoryTasksManager taskManager = Managers.getDefault();
+        Generator generator = new Generator(taskManager);
+        generator.start();
     }
 }
