c3dec2431b9c3112e65066a589546c6a01c242ca
Andrey <dev.andr@yandex.com>
2022-01-18 22:04:29 +0500

    Revert "feat: добавить методы equals(), hashcode()"
    This reverts commit 4860efad49fd43903bb1a972d3e5de01bc3039c8.

--- src/ru/yandex/practicum/tracker/tasks/SubTask.java
+++ src/ru/yandex/practicum/tracker/tasks/SubTask.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.tracker.tasks;
 
-import java.util.Objects;
-
 //класс для подзадач
 public class SubTask extends Task{
     private long epicId;
@@ -26,19 +24,5 @@ public class SubTask extends Task{
 
     public long getEpicId() {
         return epicId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        SubTask subTask = (SubTask) o;
-        return epicId == subTask.epicId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), epicId);
     }
 }
