5efe29377d20f14ab89bcbf00c8be8a0f65c228c
Andrey <dev.andr@yandex.com>
2022-01-30 17:30:35 +0500

    feat: добавить новый класс для узла

---
+++ src/ru/yandex/practicum/tracker/manager/history/Node.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.tracker.manager.history;
+
+public class Node<E> {
+
+    public E data;
+    public Node<E> next;
+    public Node<E> prev;
+
+    public Node(Node<E> prev, E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
