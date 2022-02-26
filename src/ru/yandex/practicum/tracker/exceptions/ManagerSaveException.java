9877cbcdd86da335542a06a96447cded16f7c477
Andrey <dev.andr@yandex.com>
2022-02-26 20:49:33 +0500

    feat: создать новый класс исключения

---
+++ src/ru/yandex/practicum/tracker/exceptions/ManagerSaveException.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.tracker.exceptions;
+
+public class ManagerSaveException extends Exception{
+
+    public ManagerSaveException() {
+    }
+
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}
