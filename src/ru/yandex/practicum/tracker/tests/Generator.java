318e0cb8b5f9761d228bc23e9d6778d76e2a807f
Andrey <dev.andr@yandex.com>
2022-03-14 19:09:25 +0500

    fix: изменить Generator для тестирования

--- src/ru/yandex/practicum/tracker/tests/Generator.java
+++ src/ru/yandex/practicum/tracker/tests/Generator.java
@@ -3,6 +3,9 @@ package ru.yandex.practicum.tracker.tests;
 import ru.yandex.practicum.tracker.manager.TaskManager;
 import ru.yandex.practicum.tracker.tasks.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 //класс для тестирования
 public class Generator {
     TaskManager taskManager;
@@ -23,39 +26,53 @@ public class Generator {
         long taskId1 = TaskId.getNewId();
         taskManager.addTask(new Task("Задача 1", "Собрание в 14:00",
                 taskId1,
-                State.IN_PROGRESS));
+                State.IN_PROGRESS,
+                Duration.ofMinutes(5),
+                LocalDateTime.now()));
         long taskId2 = TaskId.getNewId();
         taskManager.addTask(new Task("Задача 2", "Вынести мусор",
                 taskId2,
-                State.NEW));
+                State.NEW,
+                Duration.ofMinutes(5),
+                LocalDateTime.now()));
 
         long epicId1 = TaskId.getNewId();
         taskManager.addTask(new Epic("Эпик 1", "Отпраздновать новый год",
                 epicId1,
-                State.NEW));
+                State.NEW,
+                Duration.ofMinutes(0),
+                LocalDateTime.now()));
 
         long subTaskId1 = TaskId.getNewId();
         taskManager.addTask(new SubTask("Подзадача 1", "Купить подарки",
                 subTaskId1,
                 State.NEW,
-                epicId1));
+                epicId1,
+                Duration.ofMinutes(5),
+                LocalDateTime.now()));
 
         long subTaskId2 = TaskId.getNewId();
         taskManager.addTask(new SubTask("Подзадача 2", "Пригласить друзей",
                 subTaskId2,
                 State.NEW,
-                epicId1));
+                epicId1,
+                Duration.ofMinutes(5),
+                LocalDateTime.now()));
 
         long subTaskId3 = TaskId.getNewId();
         taskManager.addTask(new SubTask("Подзадача 3", "За продуктами",
                 subTaskId3,
                 State.NEW,
-                epicId1));
+                epicId1,
+                Duration.ofMinutes(5),
+                LocalDateTime.now()));
 
         long epicId2 = TaskId.getNewId();
         taskManager.addTask(new Epic("Эпик 2", "Убраться в квартире",
                 epicId2,
-                State.NEW));
+                State.NEW,
+                Duration.ofMinutes(0),
+                LocalDateTime.now()));
 
         System.out.println("    Запрос задачи  " + taskManager.getTask(epicId2));
         printHistory();
