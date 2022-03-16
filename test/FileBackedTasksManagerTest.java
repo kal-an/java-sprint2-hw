24f4bfaf9249ccf25a1a20dba2e369dde92616d0
Andrey <dev.andr@yandex.com>
2022-03-16 11:00:35 +0500

    feat: добавить методы тестирования FileBackedTasksManagerTest

--- test/FileBackedTasksManagerTest.java
+++ test/FileBackedTasksManagerTest.java
@@ -1,5 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.tracker.manager.FileBackedTasksManager;
+import ru.yandex.practicum.tracker.tasks.SubTask;
+import ru.yandex.practicum.tracker.tasks.Task;
+
+import java.util.List;
 
 class FileBackedTasksManagerTest {
 
+    private FileBackedTasksManager fileBackedTasksManager;
+
+    @BeforeEach
+    public void createBeforeEach() {
+        fileBackedTasksManager = FileBackedTasksManager.start();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTaskListIsEmpty() {
+        List<Task> taskList = fileBackedTasksManager.getAllTasks();
+        Assertions.assertTrue(taskList.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenEpicWithoutSubTask() {
+        List<SubTask> taskList = fileBackedTasksManager.getSubTasks(7);
+        Assertions.assertTrue(taskList.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenHistoryIsEmpty() {
+        List<Task> taskList = fileBackedTasksManager.getHistory();
+        Assertions.assertTrue(taskList.isEmpty());
+    }
 }
