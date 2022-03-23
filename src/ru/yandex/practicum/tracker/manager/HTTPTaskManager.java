eb1abf123e438af18b6fd2913d23277e9a8d1558
Andrey <dev.andr@yandex.com>
2022-03-23 10:44:42 +0500

    feat: создать конструктор для HTTPTaskManager

--- src/ru/yandex/practicum/tracker/manager/HTTPTaskManager.java
+++ src/ru/yandex/practicum/tracker/manager/HTTPTaskManager.java
@@ -1,4 +1,9 @@
 package ru.yandex.practicum.tracker.manager;
 
+import java.net.URI;
+
 public class HTTPTaskManager extends FileBackedTasksManager{
+
+    public HTTPTaskManager(URI uri) {
+    }
 }
