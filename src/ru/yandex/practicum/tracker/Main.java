69d95cdd1ce6003df1c26bce26fec382eb707f60
Andrey <dev.andr@yandex.com>
2022-03-29 00:34:42 +0500

    fix: удалить неиспользуемый класс Generator

--- src/ru/yandex/practicum/tracker/Main.java
+++ src/ru/yandex/practicum/tracker/Main.java
@@ -2,13 +2,10 @@ package ru.yandex.practicum.tracker;
 
 import ru.yandex.practicum.tracker.manager.Managers;
 import ru.yandex.practicum.tracker.manager.TaskManager;
-import ru.yandex.practicum.tracker.tests.Generator;
 
 public class Main {
     public static void main(String[] args) {
 
         TaskManager taskManager = Managers.getDefault();
-        Generator generator = new Generator(taskManager);
-        generator.start();
     }
 }
