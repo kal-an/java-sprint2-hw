ed7895beaa63152f0943caf76405fab9120a04ff
Andrey <dev.andr@yandex.com>
2022-03-29 19:08:18 +0500

    fix: убрать лишние импорты

--- src/ru/yandex/practicum/tracker/manager/HTTPTaskManager.java
+++ src/ru/yandex/practicum/tracker/manager/HTTPTaskManager.java
@@ -2,17 +2,14 @@ package ru.yandex.practicum.tracker.manager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 import ru.yandex.practicum.tracker.client.KVTaskClient;
-import ru.yandex.practicum.tracker.exceptions.ManagerSaveException;
 import ru.yandex.practicum.tracker.tasks.Task;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HTTPTaskManager extends FileBackedTasksManager{
+public class HTTPTaskManager extends FileBackedTasksManager {
 
     private final KVTaskClient kvTaskClient;
 
@@ -31,8 +28,10 @@ public class HTTPTaskManager extends FileBackedTasksManager{
     private void loadFromStorage() {
         Gson gson = new GsonBuilder().create();
         List<Task> tasks = gson.fromJson(kvTaskClient.load("tasks"),
-                new TypeToken<ArrayList<Task>>(){}.getType());
+                new TypeToken<ArrayList<Task>>() {
+                }.getType());
         List<Task> history = gson.fromJson(kvTaskClient.load("history"),
-                new TypeToken<ArrayList<Task>>(){}.getType());
+                new TypeToken<ArrayList<Task>>() {
+                }.getType());
     }
 }
