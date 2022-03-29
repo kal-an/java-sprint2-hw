62822b8d9b7a2a3091483a607263b1033e3792f1
Andrey <dev.andr@yandex.com>
2022-03-29 20:30:36 +0500

    fix: перенести запуск сервера в Main

--- src/ru/yandex/practicum/tracker/Main.java
+++ src/ru/yandex/practicum/tracker/Main.java
@@ -2,10 +2,14 @@ package ru.yandex.practicum.tracker;
 
 import ru.yandex.practicum.tracker.manager.Managers;
 import ru.yandex.practicum.tracker.manager.TaskManager;
+import ru.yandex.practicum.tracker.server.KVServer;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         TaskManager taskManager = Managers.getDefault();
+        new KVServer().start();
     }
 }
--- src/ru/yandex/practicum/tracker/server/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.tracker.server;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        new KVServer().start();
-    }
-}
