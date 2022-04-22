21861ba15e901012a7e6cbe24c8458c03923a85a
Andrey <dev.andr@yandex.com>
2022-04-23 01:35:18 +0500

    fix: оптимизировать тесты эпика

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.tracker.manager.InMemoryTasksManager;
 import ru.yandex.practicum.tracker.manager.TaskManager;
@@ -13,35 +14,30 @@ import java.time.LocalDateTime;
 // Класс тестирования для эпика
 class EpicTest {
 
-    TaskManager taskManager;
+    private TaskManager taskManager;
+    private Epic epic;
+    private SubTask subTask1;
+    private SubTask subTask2;
 
     @BeforeEach
+    @DisplayName("Создание задач")
+    @Test
     public void createBeforeEach() {
         taskManager = new InMemoryTasksManager();
-    }
-
-    @Test
-    public void shouldSetStatusNewWhenEpicCreated() {
-        Epic epic = new Epic("Эпик 1", "Отпраздновать новый год",
+        epic = new Epic("Эпик 1", "Отпраздновать новый год",
                 State.NEW);
         taskManager.addTask(epic);
+
         Assertions.assertEquals(State.NEW, epic.getTaskStatus());
-    }
 
-    @Test
-    public void shouldSetStatusEpicNewWhenNewSubTasks() {
-        Epic epic = new Epic("Эпик 1", "Отпраздновать новый год",
-                State.NEW);
-        taskManager.addTask(epic);
-
-        SubTask subTask1 = new SubTask("Подзадача 1", "Купить подарки",
+        subTask1 = new SubTask("Подзадача 1", "Купить подарки",
                 State.NEW,
                 epic.getTaskId(),
                 Duration.ofMinutes(30),
                 LocalDateTime.of(2021, 3, 19, 13, 30));
         taskManager.addTask(subTask1);
 
-        SubTask subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
+        subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
                 State.NEW,
                 epic.getTaskId(),
                 Duration.ofMinutes(10),
@@ -49,106 +45,43 @@ class EpicTest {
         taskManager.addTask(subTask2);
 
         Assertions.assertEquals(State.NEW, epic.getTaskStatus());
+        Assertions.assertEquals(3, taskManager.getAllTasks().size());
     }
 
     @Test
+    @DisplayName("Статус эпика DONE когда все подзадачи DONE")
     public void shouldSetStatusEpicNewWhenAllSubTaskIsDone() {
-        Epic epic = new Epic("Эпик 1", "Отпраздновать новый год",
-                State.NEW);
-        taskManager.addTask(epic);
-
-        SubTask subTask1 = new SubTask("Подзадача 1", "Купить подарки",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 7, 15, 13, 30));
-        taskManager.addTask(subTask1);
-
-        subTask1 = new SubTask("Подзадача 1", "Купить подарки",
-                State.DONE,
-                epic.getTaskId(),
-                Duration.ofMinutes(20),
-                LocalDateTime.of(2022, 8, 15, 16, 30));
+        subTask1.setTaskId(2);
+        subTask1.setTaskStatus(State.DONE);
         taskManager.updateTask(subTask1);
-
-        SubTask subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 9, 15, 15, 30));
-        taskManager.addTask(subTask2);
-        subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.DONE,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 10, 15, 15, 30));
+        subTask2.setTaskId(3);
+        subTask2.setTaskStatus(State.DONE);
         taskManager.updateTask(subTask2);
 
         Assertions.assertEquals(State.DONE, epic.getTaskStatus());
     }
 
     @Test
+    @DisplayName("Статус эпика IN_PROGRESS когда подзадачи со статусами NEW и DONE")
     public void shouldSetStatusEpicInProgressWhenOneSubTaskIsDone() {
-        Epic epic = new Epic("Эпик 1", "Отпраздновать новый год",
-                State.NEW);
-        taskManager.addTask(epic);
-
-        SubTask subTask1 = new SubTask("Подзадача 1", "Купить подарки",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(30),
-                LocalDateTime.of(2022, 6, 15, 13, 30));
-        taskManager.addTask(subTask1);
-
-        SubTask subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 12, 15, 15, 30));
-        taskManager.addTask(subTask2);
-
-        subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.DONE,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 3, 15, 15, 30));
+        subTask1.setTaskId(2);
+        subTask1.setTaskStatus(State.IN_PROGRESS);
+        taskManager.updateTask(subTask1);
+        subTask2.setTaskId(3);
+        subTask2.setTaskStatus(State.DONE);
         taskManager.updateTask(subTask2);
 
         Assertions.assertEquals(State.IN_PROGRESS, epic.getTaskStatus());
     }
 
     @Test
+    @DisplayName("Статус эпика IN_PROGRESS когда подзадачи со статусом IN_PROGRESS")
     public void shouldSetStatusEpicInProgressWhenAllSubTaskIsInProgress() {
-        Epic epic = new Epic("Эпик 1", "Отпраздновать новый год",
-                State.NEW);
-        taskManager.addTask(epic);
-
-        SubTask subTask1 = new SubTask("Подзадача 1", "Купить подарки",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(30),
-                LocalDateTime.of(2022, 4, 13, 13, 30));
-        taskManager.addTask(subTask1);
-
-        subTask1 = new SubTask("Подзадача 1", "Купить подарки",
-                State.IN_PROGRESS,
-                epic.getTaskId(),
-                Duration.ofMinutes(30),
-                LocalDateTime.of(2022, 3, 15, 13, 30));
+        subTask1.setTaskId(2);
+        subTask1.setTaskStatus(State.IN_PROGRESS);
         taskManager.updateTask(subTask1);
-
-        SubTask subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.NEW,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 2, 20, 15, 30));
-        taskManager.addTask(subTask2);
-
-        subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.IN_PROGRESS,
-                epic.getTaskId(),
-                Duration.ofMinutes(10),
-                LocalDateTime.of(2022, 3, 15, 15, 30));
+        subTask2.setTaskId(3);
+        subTask2.setTaskStatus(State.IN_PROGRESS);
         taskManager.updateTask(subTask2);
 
         Assertions.assertEquals(State.IN_PROGRESS, epic.getTaskStatus());
