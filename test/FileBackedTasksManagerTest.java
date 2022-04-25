254e89b93c996e87e28ec1fa79948cc28683648c
Andrey <dev.andr@yandex.com>
2022-04-25 19:15:42 +0500

    fix: оптимизировать тесты FileBackedTasksManager

--- test/FileBackedTasksManagerTest.java
+++ test/FileBackedTasksManagerTest.java
@@ -1,99 +1,45 @@
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.tracker.manager.FileBackedTasksManager;
-import ru.yandex.practicum.tracker.manager.TaskManager;
-import ru.yandex.practicum.tracker.tasks.Epic;
-import ru.yandex.practicum.tracker.tasks.State;
-import ru.yandex.practicum.tracker.tasks.SubTask;
 import ru.yandex.practicum.tracker.tasks.Task;
 
+import java.io.File;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
 
 class FileBackedTasksManagerTest extends TaskManagerTest {
 
-    static TaskManager fileBackedTasksManager;
-
-    @BeforeEach
-    public void createBeforeEach() {
-        fileBackedTasksManager = FileBackedTasksManager.start();
-    }
+    private Task task1;
+    private Task task2;
 
     @Test
-    public void shouldReturnTrueWhenTaskListIsEmpty() {
-        fileBackedTasksManager.removeTask();
-        Assertions.assertTrue(fileBackedTasksManager.getAllTasks().isEmpty());
-    }
+    @DisplayName("Сохранение и восстановление задач")
+    void saveAndLoad() {
+        final String filename = "./src/ru/yandex/practicum/tracker/test.csv";
+        File file = new File(filename);
+        taskManager = FileBackedTasksManager.loadFromFile(file);
+        taskManager.removeTask();
 
-    @Test
-    public void shouldReturnTrueWhenEpicWithoutSubTask() {
-        List<SubTask> taskList = fileBackedTasksManager.getSubTasks(7);
-        Assertions.assertTrue(taskList.isEmpty());
-    }
+        Assertions.assertEquals(0, taskManager.getAllTasks().size());
+        Assertions.assertEquals(0, taskManager.getHistory().size());
 
-    @Test
-    public void shouldReturnTrueWhenHistoryIsEmpty() {
-        fileBackedTasksManager.removeTask();
-        Assertions.assertTrue(fileBackedTasksManager.getHistory().isEmpty());
-    }
+        task1 = new Task("Задача 1", "Собрание в 14:00",
+                Duration.ofMinutes(30),
+                LocalDateTime.of(2020, 3, 15, 14, 0));
+        taskManager.addTask(task1);
 
-    @AfterAll
-    public static void createAfterAll() {
-        Task task1 = new Task("Задача 1", "Собрание в 14:00",
-                State.NEW,
+        task2 = new Task("Задача 2", "Вынести мусор",
                 Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 15, 10, 30));
-        fileBackedTasksManager.addTask(task1);
+                LocalDateTime.of(2020, 4, 15, 10, 30));
+        taskManager.addTask(task2);
+        taskManager.getTask(task1.getTaskId());
+        taskManager.getTask(task2.getTaskId());
 
-        Task task2 = new Task("Задача 2", "Вынести мусор",
-                State.NEW,
-                Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 15, 12, 30));
-        fileBackedTasksManager.addTask(task2);
+        FileBackedTasksManager newTaskManager = FileBackedTasksManager.loadFromFile(file);
 
-        Epic epic1 = new Epic("Эпик 1", "Отпраздновать новый год",
-                State.NEW);
-        fileBackedTasksManager.addTask(epic1);
-
-        fileBackedTasksManager.addTask(new SubTask("Подзадача 1",
-                "Купить подарки",
-                State.NEW,
-                epic1.getTaskId(),
-                Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 15, 13, 30)));
-
-        fileBackedTasksManager.addTask(new SubTask("Подзадача 2",
-                "Пригласить друзей",
-                State.NEW,
-                epic1.getTaskId(),
-                Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 15, 15, 30)));
-
-        SubTask subTask2 = new SubTask("Подзадача 2", "Пригласить друзей",
-                State.IN_PROGRESS,
-                epic1.getTaskId(),
-                Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 15, 15, 30));
-        fileBackedTasksManager.updateTask(subTask2);
-
-        fileBackedTasksManager.addTask(new SubTask("Подзадача 3",
-                "За продуктами",
-                State.NEW,
-                epic1.getTaskId(),
-                Duration.ofMinutes(5),
-                LocalDateTime.of(2022, 3, 14, 2, 30)));
-
-        Epic epic2 = new Epic("Эпик 2", "Убраться в квартире",
-                State.NEW);
-        fileBackedTasksManager.addTask(epic2);
-
-        fileBackedTasksManager.getTask(task1.getTaskId());
-        fileBackedTasksManager.getTask(subTask2.getTaskId());
-        fileBackedTasksManager.getTask(task2.getTaskId());
-        fileBackedTasksManager.getTask(epic1.getTaskId());
-
+        Assertions.assertEquals(2, newTaskManager.getAllTasks().size());
+        Assertions.assertEquals(taskManager.getHistory().size(),
+                newTaskManager.getHistory().size());
     }
 }
